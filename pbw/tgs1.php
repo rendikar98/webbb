eef75cc0928d05bd12df4ef3deea24b15e2afaf5
rendikar98 <rendikar98@gmail.com>
2023-02-15 20:24:57 +0700

    feat: add some file

---
+++ pbw/tgs1.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en"></html>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BIODATA</title>
+    <head>
+        <style>
+            table, th, td {
+                border: 1px solid black;
+                border-collapse: collapse;
+            }
+
+            th, td {
+                text-align: left;
+                padding: 8px;
+            }
+
+
+            tr:nth-child(even) {
+                background-color: #D6EEEE;
+            }
+        </style>
+        <body>
+            <h1>Biodata</h1>
+            <p>Nama lengkap: Rendika Rahmaturrizki</p>
+            <p>NIM: 2108107010066</p>
+            <p>Jurusan: Informatika</p>
+            <p>Fakultas: MIPA</p>
+            <p>Universitas: Universitas Syiah Kuala</p>
+
+            <table>
+                <tr>
+                    <th>Nama Mata Kuliah</th>
+                    <th>SKS</th>
+                    <th>Dosen Pengampu</th>
+                    <th>Ruangan</th>
+                    <th>Waktu & Hari</th>
+                </tr>
+                <tr>
+                    <th>Data Visualization</th>
+                    <th>3</th>
+                    <th>Mrs. Laina</th>
+                    <th>03.03</th>
+                    <th>Haha its xMonday at 2 P.M</th>
+                </tr>
+                <tr>
+                    <th>Graph Theory</th>
+                    <th>2</th>
+                    <th>Mrs. Dalila</th>
+                    <th>03.02</th>
+                    <th>Chewsday Innit at 2 P.M</th>
+                </tr>
+                <tr>
+                    <th>Algorithm and Data Structure</th>
+                    <th>3</th>
+                    <th>Sir Zahnur</th>
+                    <th>03.02</th>
+                    <th>Its monday ma dudes at 8 A.M</th>
+                </tr>
+                <tr>
+                    <th>Software Engineering</th>
+                    <th>3</th>
+                    <th>Mrs. Dalila</th>
+                    <th>SE Lab</th>
+                    <th>yea its Friday bois at 8 A.M</th>
+                </tr>
+                <tr>
+                    <th>Web Programming</th>
+                    <th>3</th>
+                    <th>Mr. Husaini & Mr. Alim</th>
+                    <th>03.02</th>
+                    <th>haha Trustsday at 2 A.M</th>
+                </tr>
+                <tr>
+                    <th>Data Communication</th>
+                    <th>3</th>
+                    <th>Mr. Husaini</th>
+                    <th>03.02</th>
+                    <th>yeaa its Friday bois at 4 P.M</th>
+                </tr>
+                <tr>
+                    <th>Geographic Information System</th>
+                    <th>3</th>
+                    <th>Sir Ardiansyah</th>
+                    <th>03.02</th>
+                    <th>Chewsday Innit at 8 A.M</th>
+                </tr>
+                <tr>
+                    <th>Informatic and Tech Ethics</th>
+                    <th>2</th>
+                    <th>Mrs. Dalila & Mr. Husaini</th>
+                    <th>03.02</th>
+                    <th>Its Wednesday ma dud at 4 P.M</th>
+                </tr>
+            </table>
+        </body>
+    </head>
